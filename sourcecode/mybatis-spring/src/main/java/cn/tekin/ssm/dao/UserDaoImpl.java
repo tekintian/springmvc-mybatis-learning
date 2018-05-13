08b462c0b4724ab84be07ce0216513972bf17706
Tekin Tian <tekintian@gmail.com>
2018-05-13 23:27:37 +0800

    spring-mybatis  maven 改造

--- sourcecode/mybatis-spring/src/main/java/cn/tekin/ssm/dao/UserDaoImpl.java
+++ sourcecode/mybatis-spring/src/main/java/cn/tekin/ssm/dao/UserDaoImpl.java
@@ -9,7 +9,6 @@ import org.mybatis.spring.support.SqlSessionDaoSupport;
  */
 public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao {
 
-    @Override
     public User findUserById(int id) throws Exception {
         //继承SqlSessionDaoSupport，通过this.getSqlSession()得到sqlSessoin
         SqlSession sqlSession = this.getSqlSession();
